8061d1ea9e1c595daa79aff2afc8bd10d1625e23
Ross Cooper <therossjcooper@gmail.com>
2016-09-03 23:13:58 +0100

    Fix for wp_title on homepage

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+add_filter( 'wp_title', 'mwd_fixed_home_wp_title' );
+ 
+/**
+ * Customize the title for the home page, if one is not set.
+ *
+ * @param string $title The original title.
+ * @return string The title to use.
+ */
+function mwd_fixed_home_wp_title( $title )
+{
+  if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
+    $title = __( 'Home', 'textdomain' ) . ' | ' . get_bloginfo( 'description' );
+  }
+  return $title;
+}
--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
+	<title><?php wp_title(); ?></title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
