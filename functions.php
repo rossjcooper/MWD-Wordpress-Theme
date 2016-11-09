bfcf5deedf4f89a939350bd04161560a44498328
Ross Cooper <ross.cooper@teknet.org.uk>
2016-11-09 11:44:15 +0000

    Added boilerplate code to include style and script asset files

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ if (file_exists(get_template_directory() . '/vendor/autoload.php')) {
 }
 
 add_filter('wp_title', 'mwd_fixed_home_wp_title');
+add_action('wp_enqueue_scripts', 'mwd_enqueue_styles');
+add_action('wp_enqueue_scripts', 'mwd_enqueue_scripts');
 
 /**
  * Customize the title for the home page, if one is not set.
@@ -18,3 +20,33 @@ function mwd_fixed_home_wp_title($title)
     }
     return $title;
 }
+
+/**
+ * Enqueues styles for the theme.
+ */
+function mwd_enqueue_styles()
+{
+    wp_enqueue_style('theme-screen-style', get_template_directory_uri() . '/public/css/screen' . mwd_get_asset_suffix() . '.css');
+    wp_enqueue_style('theme-print-style', get_template_directory_uri() . '/public/css/print' . mwd_get_asset_suffix() . '.css');
+}
+
+/**
+ *  Enqueues the scripts for the theme.
+ */
+function mwd_enqueue_scripts()
+{
+    wp_enqueue_script('theme-script', get_template_directory_uri() . '/public/js/main' . mwd_get_asset_suffix() . '.js', array('jquery'), '1.0.0', true);
+}
+
+/**
+ * Returns '.min' if Wordpress is not in DEBUG mode.
+ *
+ * Useful for including production files when DEBUG is off.
+ *
+ * @return string '.min' if Debug is on otherwise empty string.
+ */
+function mwd_get_asset_suffix()
+{
+    return (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? '' : '.min';
+}
+
