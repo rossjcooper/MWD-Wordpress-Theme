7ae539b9332e43bc1c3781cf14efd69fc67866c5
Ross Cooper <ross.cooper@teknet.org.uk>
2016-11-09 11:11:33 +0000

    Included composer autoloader file in functions files if it exists

--- functions.php
+++ functions.php
@@ -1,17 +1,20 @@
-<?php 
+<?php
+if (file_exists(get_template_directory() . '/vendor/autoload.php')) {
+    include get_template_directory() . '/vendor/autoload.php';
+}
 
-add_filter( 'wp_title', 'mwd_fixed_home_wp_title' );
- 
+add_filter('wp_title', 'mwd_fixed_home_wp_title');
+
 /**
  * Customize the title for the home page, if one is not set.
  *
  * @param string $title The original title.
  * @return string The title to use.
  */
-function mwd_fixed_home_wp_title( $title )
+function mwd_fixed_home_wp_title($title)
 {
-  if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
-    $title = __( 'Home', 'textdomain' ) . ' | ' . get_bloginfo( 'description' );
-  }
-  return $title;
+    if (empty($title) && (is_home() || is_front_page())) {
+        $title = __('Home', 'textdomain') . ' | ' . get_bloginfo('description');
+    }
+    return $title;
 }
